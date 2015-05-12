ba29eb9368fd4e6a8b29690ef5c82830227ba542
pfj3 <pfj@outlook.com>
2015-05-12 12:59:36 -0400

    Initial commit

---
+++ CopiableLabelSubclass/UICopiableLabel.swift
@@ -0,0 +1,42 @@
+//
+//  UICopiableLabel.swift
+//  CopiableLabelSubclass
+//
+//  Created by Peter Johnson on 5/12/15.
+//  Copyright (c) 2015 Peter Johnson. All rights reserved.
+//
+
+import UIKit
+
+class UICopiableLabel: UILabel {
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.userInteractionEnabled = true
+        
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: Selector("longPress"))
+        self.addGestureRecognizer(longPressGesture)
+        
+    }
+    
+    func longPress() {
+        println("Long press detected")
+        becomeFirstResponder()
+        let theMenu = UIMenuController.sharedMenuController()
+        let copyItem = UIMenuItem(title: "Copy", action: Selector("copyToClipboard"))
+        theMenu.menuItems = [copyItem]
+        theMenu.setTargetRect(self.bounds, inView: self)
+        theMenu.setMenuVisible(true, animated: true)
+    }
+    
+    func copyToClipboard() {
+        println("Copy called")
+        UIPasteboard.generalPasteboard().string = self.text
+    }
+
+}
